b3c1a7f225d49b09a1e3de2a5bf77cb98dda4447
lukeaparker <52385078+lukeaparker@users.noreply.github.com>
2022-05-20 15:54:35 -0400

    Update scripts.js
    Updated year

--- scripts.js
+++ scripts.js
@@ -9,7 +9,7 @@
         mm = String(today.getMonth() + 1).padStart(2, "0"),
         yyyy = today.getFullYear(),
         nextYear = yyyy + 1,
-        dayMonth = "12/28/",
+        dayMonth = "6/1/",
         birthday = dayMonth + yyyy;
     
     today = mm + "/" + dd + "/" + yyyy;
